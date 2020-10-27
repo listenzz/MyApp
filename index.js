b99d60ad425a989032e1703ff732c73f12605a44
listen <listenzz@163.com>
2020-10-27 12:11:09 +0800

    升级依赖

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 import App from './app/App'
 import Blank from './app/Blank'
 import { AppState } from 'react-native'
-import { commit, version_code } from './app.json'
+import { commit } from './app.json'
 import { ReactRegistry, Garden, Navigator } from 'react-native-navigation-hybrid'
 import * as Sentry from '@sentry/react-native'
-import {
-  APPLICATION_ID,
-  ENVIRONMENT,
-  BUILD_TYPE,
-  BUILD_TYPE_RELEASE,
-  VERSION_CODE,
-  VERSION_NAME,
-} from './app/AppInfo'
+import { APPLICATION_ID, ENVIRONMENT, BUILD_TYPE, BUILD_TYPE_RELEASE } from './app/AppInfo'
 import CodePush from 'react-native-code-push'
 
 if (BUILD_TYPE === BUILD_TYPE_RELEASE) {
   Sentry.init({
     dsn: 'https://2d17bb1ffde34fec963d29b4c3a29f99@sentry.io/1446147',
     environment: ENVIRONMENT,
-    // for native code
-    release: `${APPLICATION_ID}-${VERSION_NAME}`,
-    dist: `${VERSION_CODE}`,
   })
 
   Sentry.setTag('commit', commit)
-  // for react code
-  Sentry.setRelease(`${APPLICATION_ID}-${VERSION_NAME}`)
-  // 如果是热更新 version_code != VERSION_CODE
-  Sentry.setDist(`${version_code}`)
 
   CodePush.getUpdateMetadata()
     .then((update) => {
