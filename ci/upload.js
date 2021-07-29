c41ba1bc5a22478f77a313e44b853301cb66b6ad
listen <listenzz@163.com>
2021-07-29 17:13:59 +0800

    发布 ipa 到自建文件服务器 [skip ci]

--- ci/upload.js
+++ ci/upload.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const { sh, slack } = require('./utils')
 const {
   ENVIRONMENT,
@@ -12,6 +13,7 @@ const {
   ARTIFACTS_DIR,
   BUGLY_DIST_URL,
   PATCH_ONLY,
+  APPLICATION_ID,
 } = require('./config')
 
 if (PATCH_ONLY) {
@@ -29,13 +31,69 @@ if (PLATFORM === 'ios') {
   if (ENVIRONMENT === 'production') {
     sh('bundle exec fastlane upload_ipa_to_testflight', { cwd: workdir })
   } else {
-    const result = sh('bundle exec fastlane upload_ipa_to_bugly', { cwd: workdir, stdio: 'pipe', encoding: 'utf8' })
-    console.log(result)
-    if (result.includes('"rtcode":0')) {
-      slack(`ios-${APP_NAME}-${ENVIRONMENT} 有新的版本了，${BUGLY_DIST_URL}`)
+    if (process.env.BUGLY_ENABLED === 'true') {
+      const result = sh('bundle exec fastlane upload_ipa_to_bugly', { cwd: workdir, stdio: 'pipe', encoding: 'utf8' })
+      console.log(result)
+      if (result.includes('"rtcode":0')) {
+        slack(`ios-${APP_NAME}-${ENVIRONMENT} 有新的版本了，${BUGLY_DIST_URL}`)
+      } else {
+        console.log('%c上传到 bugly 失败', 'color:"#FF4444"; font-size:24px')
+        process.exit(1)
+      }
     } else {
-      console.log('%c上传到 bugly 失败', 'color:"#FF4444"; font-size:24px')
-      process.exit(1)
+      const file = path.join(ARTIFACTS_DIR, `${ENVIRONMENT}-${VERSION_NAME}-${VERSION_CODE}`)
+      let filename = `${ENVIRONMENT}-${VERSION_NAME}-${VERSION_CODE}`
+
+      if (process.env.CI_BUILD_REF_SLUG) {
+        filename = `${process.env.CI_BUILD_REF_SLUG}-${ENVIRONMENT}-${VERSION_NAME}-${VERSION_CODE}`
+      }
+      const dest = `ios/${APP_NAME}/${ENVIRONMENT}/${VERSION_NAME}`
+
+      const plist = fs
+        .readFileSync(path.resolve(__dirname, './template/ipa.plist'), 'utf8')
+        .replace('{IpaUrl}', `${FILE_SERVER}/${dest}/${filename}.ipa`)
+        .replace(/{IconUrl}/g, `${FILE_SERVER}/${APP_NAME}/${ENVIRONMENT}/icon.png`)
+        .replace('{BundleIdentifier}', APPLICATION_ID)
+        .replace('{BundleVersion}', VERSION_NAME)
+        .replace('{AppName}', APP_NAME)
+
+      fs.writeFileSync(`${file}.plist`, plist)
+
+      const html = fs
+        .readFileSync(path.resolve(__dirname, './template/ipa.html'), 'utf8')
+        .replace('{AppName}', APP_NAME)
+        .replace('{PListUrl}', `${FILE_SERVER}/${dest}/${filename}.plist`)
+        .replace('{AppVersion}', `${APP_NAME}-${filename}`)
+        .replace('{HtmlUrl}', `${FILE_SERVER}/${dest}/${filename}.html`)
+        .replace('{ServerUrl}', `${FILE_SERVER}/${dest}/`)
+      fs.writeFileSync(`${file}.html`, html)
+
+      uploadFile(`${file}.ipa`, `${filename}.ipa`, dest)
+      uploadFile(`${file}.plist`, `${filename}.plist`, dest)
+      uploadFile(`${file}.html`, `${filename}.html`, dest)
+
+      slack(`ios-${APP_NAME}-${ENVIRONMENT} 有新的版本了，${FILE_SERVER}/${dest}/${filename}.html`)
+
+      function uploadFile(file, filename, dest) {
+        console.info(`准备上传 ${filename}`)
+
+        const result = sh(`curl -F file=@${file} -F filename=${filename} ${FILE_SERVER}/${dest}`, {
+          stdio: 'pipe',
+          encoding: 'utf8',
+        })
+
+        try {
+          console.info(`上传结果 ${result}`)
+          const res = JSON.parse(result)
+          if (res && res.success) {
+          } else {
+            process.exit(1)
+          }
+        } catch (e) {
+          console.warn(e.message)
+          process.exit(1)
+        }
+      }
     }
   }
   process.exit(0)
