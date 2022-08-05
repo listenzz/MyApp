a4a5496160b8622dd6a0b635cb9ecf4789d80073
listen <listenzz@163.com>
2022-08-05 14:48:07 +0800

    升级 vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,15 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
 module.exports = {
   title: 'React Native 工程实践',
   description: 'React Native CI/CD, 工程实践',
   base: '/rn/devops/',
-  themeConfig: {
+  theme: defaultTheme({
     home: '/',
     lastUpdatedText: '上次更新',
-    sidebarDepth: 2,
     contributors: false,
     repo: 'https://github.com/listenzz/MyApp',
     editLink: false,
-    docsBranch: 'master',
     navbar: [
       { text: '首页', link: '/', },
       { text: 'React Native 开发指南', link: 'https://todoit.tech/' },
@@ -25,8 +25,8 @@ module.exports = {
         'hotfix',
       ]
     }
-  },
-
+  }),
+ 
   markdown: {
     code: {
       lineNumbers: false
